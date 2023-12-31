1fc7dd3b1b9ba721dd702596cfb2bb451783de4e
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2023-12-31 17:57:52 +0000

    ainda mais progresso

--- source/menu.cpp
+++ source/menu.cpp
@@ -70,6 +70,34 @@ int menu::print_menu_and_get_number_input(string current_selection) {
     return input;
 }
 
+string menu::print_menu_and_get_string_input(string current_selection) {
+    string input;
+    bool valid_input = false;
+    while (!valid_input) {
+        cout << current_selection << endl;
+
+        this->print_separator();
+
+        cout << "Enter a string and press enter\n";
+
+        this->print_separator();
+
+        cout << "0. Go back to previous menu\n";
+        cout << "-1. Go back to main menu\n";
+
+        this->print_separator();
+
+        cout << "Chose one of the options above by entering the corresponding number and press enter\n";
+        cin >> input;
+        if (input != "") {
+            valid_input = true;
+        } else {
+            cout << "Invalid input, please try again\n";
+        }
+    }
+    return input;
+}
+
 
 menu::menu() {
     Request = request();
@@ -111,7 +139,7 @@ void menu::statistics_of_the_network() {
             case 0:
                 return;
             case 1:
-//                this->global_statistics();
+                this->global_statistics();
                 break;
             case 2:
 //                this->statistics_for_a_specific_airport();
@@ -129,3 +157,38 @@ void menu::statistics_of_the_network() {
     }
 }
 
+void menu::global_statistics() {
+    while (!this->go_back_to_main_menu) {
+        string current_menu = "Global statistics";
+        vector<string> options = {"Total number of airports", "Total number of available flights",
+                                  "Airports with top air traffic capacity",
+                                  "Airports that are essential to the network",
+                                  "Maximum trip and corresponding pair of source-destination airports"};
+        int input = this->print_menu_and_get_choice_input(current_menu, options);
+        switch (input) {
+            case -1:
+                this->go_back_to_main_menu = true;
+                return;
+            case 0:
+                return;
+            case 1:
+//                this->get_total_number_of_airports();
+                break;
+            case 2:
+//                this->get_total_number_of_available_flights();
+                break;
+            case 3:
+//                this->get_airports_with_top_air_traffic_capacity();
+                break;
+            case 4:
+//                this->get_airports_that_are_essential_to_the_network();
+                break;
+            case 5:
+//                this->get_maximum_trip_and_corresponding_pair_of_source_destination_airports();
+                break;
+            default:
+                cout << "Invalid input, please try again\n";
+                break;
+        }
+    }
+}
